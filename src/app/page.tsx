b827fa55183a173a5353d264a31a83f422513408
DFtorres <dftorres2004@gmail.com>
2025-01-18 18:26:04 -0500

    Fix import error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Contact } from "@/components/Home/Contact";
+import Contact from "@/components/Home/Contact";
 import { Hero } from "@/components/Home/Hero";
 import { Info } from "@/components/Home/Info";
 import { Services } from "@/components/Home/Services";
