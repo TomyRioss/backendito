3e488c320c8a54ed0c20e23f81cc230afe0c28e5
Facundo Pettersson <visualstudio.creator23@gmail.com>
2025-01-18 12:06:59 -0300

    refactor: :building_construction: Changes in file and folder structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
 import NavBar from '@/components/Layout/Navbar';
+import { Footer } from '@/components/Layout/Footer';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -28,6 +29,8 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <NavBar />
         {children}
+        <Footer />
+
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ import { Contact } from "@/components/Home/Contact";
 import { Hero } from "@/components/Home/Hero";
 import { Info } from "@/components/Home/Info";
 import { Services } from "@/components/Home/Services";
-import { Footer } from "@/components/Layout/Footer";
 
 
 export default function Home() {
@@ -12,7 +11,6 @@ export default function Home() {
       <Contact />
       <Info />
       <Services />
-      <Footer />
     </>
   );
 }
