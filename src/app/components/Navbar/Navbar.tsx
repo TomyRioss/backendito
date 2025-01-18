49c12aa4c8f2a880fc4f2e45767b0b73cb4c3471
DFtorres <dftorres2004@gmail.com>
2025-01-18 16:22:09 -0500

    Added a toggler button for changing the theme and a demo button

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -1,9 +1,20 @@
-import Link from 'next/link';
-import { menusList } from './menusList';
+"use client";
+import Link from "next/link";
+import { menusList } from "./menusList";
+import { useContext } from "react";
+import { ThemeContext } from "@/shared/styles/themes/themeProvider";
+import DemoThemeButton from "@/components/ui/customButton";
 
 function NavBar() {
+  const themeContext: { isDarkMode?: boolean; toggleThemeHandler: () => void } =
+    useContext(ThemeContext);
+
+  const toggleThemeHandler = (): void => {
+    themeContext.toggleThemeHandler();
+  };
+
   return (
-    <header className="flex justify-around items-center w-full h-16 bg-blue-700 px-10 py-4">
+    <header className="flex justify-around items-center w-full h-16 bg-blue-700 dark:bg-black px-10 py-4">
       <div className="flex gap-5">
         <Link href="/">
           <h2 className="text-2xl text-white hover:text-blue-300 transition duration-300">
@@ -26,6 +37,12 @@ function NavBar() {
               <span className="absolute bottom-0 left-0 w-0 h-[2px] bg-blue-300 transition-all duration-300 hover:w-full"></span>
             </li>
           ))}
+          <li className="relative text-white hover:text-blue-300 transition duration-300">
+            <button onClick={toggleThemeHandler}>Change theme</button>
+          </li>
+          <li>
+            <DemoThemeButton />
+          </li>
         </ul>
       </nav>
     </header>
