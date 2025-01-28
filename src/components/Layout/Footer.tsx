dd354d47d771e196960edc2459937d695f9bbcca
tomy <tomyrios2006@gmail.com>
2025-01-27 22:34:20 -0300

    language bugs fixed

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -10,7 +10,7 @@ import Link from 'next/link';
 import { useTranslations } from 'next-intl';
 
 export const Footer = () => {
-  const t = useTranslations('footer');
+  const t = useTranslations('landingPage.footer');
 
   const socialLinks = [
     {
