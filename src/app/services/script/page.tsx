164536528b04e4c98f8c44fe7d96b57dec87e3fd
Angel <99usui99@gmail.com>
2025-01-28 00:49:38 -0400

    Se arreglo el responsive de servicios web

--- src/app/services/script/page.tsx
+++ src/app/services/script/page.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import Planes from './PlanesScript';
-import WorkInfo from '../pagesContent/WorkInfo';
-import PaymentInfo from '../pagesContent/PaymentInfo';
-import Link from 'next/link';
-import { FaArrowLeft } from 'react-icons/fa';
+import React from "react";
+import Planes from "./PlanesScript";
+import WorkInfo from "../pagesContent/WorkInfo";
+import PaymentInfo from "../pagesContent/PaymentInfo";
+import Link from "next/link";
+import { FaArrowLeft } from "react-icons/fa";
 
 const page = () => {
   return (
     <>
-      <Link href={'/services'}>
+      <Link href={"/services"}>
         <FaArrowLeft className="absolute right-20 left-4 top-20 h-10 w-10 rounded-xl text-red-gray-800 hover:text-red-gray-600 transition-transform transform hover:scale-125 cursor-pointer dark:text-white" />
       </Link>
       <header className="  dark:bg-gray-900">
@@ -16,7 +16,7 @@ const page = () => {
           <h1 className="text-4xl mb-5 dark:text-white mt-20">
             Scripting Personalizado
           </h1>
-          <h3 className="text-gray-700 text-xl text-center px-4 w-1/2 dark:text-white">
+          <h3 className="text-gray-700 text-xl text-center px-4 w-full md:w-1/2 dark:text-white">
             Nuestros planes de scripting están diseñados para empresas y
             profesionales que necesitan soluciones personalizadas para
             automatización, desarrollo y optimización de procesos mediante
